177e8d2f6ad42f8691da0c91952f3a9609842caf
lemon <lm123abc9@163.com>
2020-03-06 22:06:11 +0800

    工程初始化

---
+++ src/main/java/org/lemon/study/App.java
@@ -0,0 +1,13 @@
+package org.lemon.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
